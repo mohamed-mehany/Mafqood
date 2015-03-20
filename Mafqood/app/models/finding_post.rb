18f2fa04c8456634fb9149efb8fa9c10533b705c
ebramsherif <ebram.sherif@gmail.com>
2015-03-20 02:20:25 +0200

    New branch for renamed controller

---
+++ Mafqood/app/models/finding_post.rb
@@ -0,0 +1,9 @@
+class FindingPost < ActiveRecord::Base
+	belongs_to :user
+	validates_presence_of :name,:description,:image,:age,:location,:gender,:contact_info
+	validates_inclusion_of :age, :in => 0..140
+  	validates :contact_info, 
+            	  :numericality => { :only_integer => true }, 
+            	  :length => { :is => 11 }	
+	mount_uploader :image, PhotoUploader
+end
--- Mafqood/db/schema.rb
+++ Mafqood/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316154643) do
+ActiveRecord::Schema.define(version: 20150319224802) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -45,6 +45,32 @@ ActiveRecord::Schema.define(version: 20150316154643) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "finding_posts", force: :cascade do |t|
+    t.string   "name",          limit: 255
+    t.string   "description",   limit: 255
+    t.string   "location",      limit: 255
+    t.string   "special_signs", limit: 255
+    t.integer  "age",           limit: 4
+    t.string   "image",         limit: 255
+    t.string   "contact_info",  limit: 255
+    t.boolean  "gender",        limit: 1
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "findings", force: :cascade do |t|
+    t.string   "contact_info",  limit: 255
+    t.string   "photo",         limit: 255
+    t.string   "location",      limit: 255
+    t.boolean  "gender",        limit: 1
+    t.string   "special_signs", limit: 255
+    t.string   "description",   limit: 255
+    t.string   "name",          limit: 255
+    t.integer  "age",           limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "suspect_posts", force: :cascade do |t|
     t.string   "approximate_age", limit: 255
