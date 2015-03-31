24427e47e5d07eabd7c19c9a63196c547fc75b41
Sherif <Sheriffahmed21@gmail.com>
2015-04-01 00:54:05 +0200

    Search tool update3

--- Mafqood/config/routes.rb
+++ Mafqood/config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :suspect_posts,:finding_posts
+  
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
