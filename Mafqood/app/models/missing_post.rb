a34e1410e49702d5f15944c5e4872a3789714164
omar <omarsharkawy94@gmail.com>
2015-04-09 01:32:01 +0200

    merging with master

--- Mafqood/app/models/missing_post.rb
+++ Mafqood/app/models/missing_post.rb
@@ -1,19 +1,25 @@
 class MissingPost < ActiveRecord::Base
 
-  attr_accessor :image
-  mount_uploader :image, ImageUploader
+  # attr_accessor :image
+  # mount_uploader :image, ImageUploader
+  # belongs_to :user
+  # validates :gender , presence: true
+  # validates :image , presence: true
+  # validates :age , presence: true
+  # validates :location , presence: true
+  # validates :reporter_name , presence: true
+  # validates :reporter_phone , presence: true
+  # validates :description, :special_signs,
+  #           :length => { :maximum => 500 }
+  # validates :reporter_phone,
+  #           :numericality => { :only_integer => true },
+  #           :length => { :is => 11 },
+  #           :format => { :with => /\A01\d\d\d\d\d\d\d\d\d\z/ }
   belongs_to :user
-  validates :gender , presence: true
-  validates :image , presence: true
-  validates :age , presence: true
-  validates :location , presence: true
-  validates :reporter_name , presence: true
-  validates :reporter_phone , presence: true
-  validates :description, :special_signs,
-            :length => { :maximum => 500 }
+  validates_presence_of :reporter_name,:description,:image,:age,:location,:gender,:reporter_phone
+  validates_inclusion_of :age, :in => 0..140
   validates :reporter_phone,
-            :numericality => { :only_integer => true },
-            :length => { :is => 11 },
-            :format => { :with => /\A01\d\d\d\d\d\d\d\d\d\z/ }
-
+                :numericality => { :only_integer => true },
+                :length => { :is => 11 }
+  mount_uploader :image, ImageUploader
 end
---
+++ Mafqood/spec/factories/user.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :missing_post do |u|
+    u.id 1
+  end
+end
