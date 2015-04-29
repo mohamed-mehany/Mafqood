5689d92acd15db57f2a245f88b5db5dacaedb2e6
ebramsherif <ebram.sherif@gmail.com>
2015-04-29 22:27:33 +0300

    Fixed view of missing post & model to fit changes

--- Mafqood/app/models/finding_post.rb
+++ Mafqood/app/models/finding_post.rb
@@ -1,7 +1,7 @@
 class FindingPost < ActiveRecord::Base
   belongs_to :user
 
-  validates_presence_of :name, :description, :image, :age, :location, :contact_info
+  validates_presence_of :name, :description, :image, :age, :location
   validates_inclusion_of :gender, in: [true, false]
   validates_inclusion_of :age, in: 0..140 
   validates :contact_info,
